cb2b7b2e66458b198e338e17916d4ef9aaab140e
張 建文 <chang180@gmail.com>
2020-07-04 11:36:47 +0800

    practice finished

--- index.php
+++ index.php
@@ -1,4 +1,4 @@
-﻿<?php include_once "base.php";?>
+﻿<?php include_once "base.php"; ?>
 <!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
 <!-- saved from url=(0040)http://127.0.0.1/test/exercise/collage/? -->
 <html xmlns="http://www.w3.org/1999/xhtml">
@@ -27,51 +27,97 @@
 				<div id="menuput" class="dbor">
 					<!--主選單放此-->
 					<span class="t botli">主選單區</span>
+					<?php
+					$menu = $Menu->all(['sh' => 1, 'parent' => 0]);
+					foreach ($menu as $m) {
+						echo "<div class='mainmu cent'>";
+						echo "<a href='" . $m['text'] . ".'>" . $m['name'] . "</a>";
+
+						$chk = $Menu->count(['parent' => $m['id']]);
+						if ($chk > 0) {
+							$sub = $Menu->all(['parent' => $m['id']]);
+							echo "<div class='mw'>";
+							foreach ($sub as $su) {
+								echo "<div class='mainmu2 cent'>";
+								echo "<a href='" . $su['text'] . "'>" . $su['name'] . "</a>";
+								echo "</div>";
+							}
+							echo "</div>";
+						}
+						echo "</div>";
+					}
+
+					?>
 				</div>
 				<div class="dbor" style="margin:3px; width:95%; height:20%; line-height:100px;">
-					<span class="t">進站總人數 :<?=$total['total'];?> </span>
+					<span class="t">進站總人數 :<?= $total['total']; ?> </span>
 				</div>
 			</div>
 			<?php
-$do=$_GET['do']??"main";
-$file="./front/".$do.".php";
-include file_exists($file)?$file:"front/main.php";
+			$do = $_GET['do'] ?? "main";
+			$file = "./front/" . $do . ".php";
+			include file_exists($file) ? $file : "front/main.php";
 
 			?>
-			
-			<div class="di di ad" style="height:540px; width:23%; padding:0px; margin-left:22px; float:left; ">
-				<!--右邊-->
-				<button style="width:100%; margin-left:auto; margin-right:auto; margin-top:2px; height:50px;" onclick="lo('?do=login')">管理登入</button>
-				<div style="width:89%; height:480px;" class="dbor">
-					<span class="t botli">校園映象區</span>
-					<script>
-						var nowpage = 0,
-							num = 0;
 
-						function pp(x) {
-							var s, t;
-							if (x == 1 && nowpage - 1 >= 0) {
-								nowpage--;
-							}
-							if (x == 2 && (nowpage + 1) * 3 <= num * 1 + 3) {
-								nowpage++;
-							}
-							$(".im").hide()
-							for (s = 0; s <= 2; s++) {
-								t = s * 1 + nowpage * 1;
-								$("#ssaa" + t).show()
-							}
+			<div class="di di ad" style="height:540px; width:23%; padding:0px; margin-left:22px; float:left; ">
+				<?php
+				if (empty($_SESSION['login'])) {
+
+
+				?>
+					<!--右邊-->
+					<button style="width:100%; margin-left:auto; margin-right:auto; margin-top:2px; height:50px;" onclick="lo('?do=login')">管理登入</button>
+					<div style="width:89%; height:480px;" class="dbor">
+
+					<?php } else { ?>
+
+						<button style="width:100%; margin-left:auto; margin-right:auto; margin-top:2px; height:50px;" onclick="lo('admin.php')">返回管理</button>
+						<div style="width:89%; height:480px;" class="dbor">
+
+						<?php } ?>
+
+						<span class="t botli">校園映象區</span>
+						<div class="cent" onclick="pp(1)"><img src="icon/up.jpg" alt=""></div>
+						<?php
+						$img = $Image->all(['sh' => 1]);
+						// var_dump($img);
+						foreach ($img as $key => $im) {
+
+						?>
+							<div class="cent im" id="ssaa<?= $key; ?>"><img src="img/<?= $im['name']; ?>" style="width:150px;height:103px;border:3px solid orange;margin:3px"></div>
+						<?php
 						}
-						pp(1)
-					</script>
-				</div>
+						?>
+						<div class="cent" onclick="pp(2)"><img src="icon/dn.jpg" alt=""></div>
+						<script>
+							var nowpage = 0,
+								num = <?= count($img); ?>;
+
+							function pp(x) {
+								var s, t;
+								if (x == 1 && nowpage - 1 >= 0) {
+									nowpage--;
+								}
+								if (x == 2 && (nowpage + 1) * 3 <= num * 1 + 3) {
+									nowpage++;
+								}
+								$(".im").hide()
+								for (s = 0; s <= 2; s++) {
+									t = s * 1 + nowpage * 1;
+									$("#ssaa" + t).show()
+								}
+							}
+							pp(1)
+						</script>
+						</div>
+					</div>
+			</div>
+			<div style="clear:both;"></div>
+			<div style="width:1024px; left:0px; position:relative; background:#FC3; margin-top:4px; height:123px; display:block;">
+				<span class="t" style="line-height:123px;"><?= $bottom['bottom']; ?></span>
 			</div>
 		</div>
-		<div style="clear:both;"></div>
-		<div style="width:1024px; left:0px; position:relative; background:#FC3; margin-top:4px; height:123px; display:block;">
-			<span class="t" style="line-height:123px;"><?=$bottom['bottom'];?></span>
-		</div>
-	</div>
 
 </body>
 
